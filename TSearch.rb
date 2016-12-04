9a78a1706f1185edcd85350714d5d1c6d33525ae
Tokaji_Nanami_e5428 <e5428@gm.kochi-ct.jp>
2016-12-04 17:58:49 +0900

    2016_12_04
    このプログラムで検索できるのはスクリーンネームに｢高知高専｣が入っているユーザー

--- TSearch.rb
+++ TSearch.rb
@@ -1,11 +1,15 @@
-require Twitter
+require 'rubygems'
+require 'twitter'
 
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
  client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = q6YVpFPdsdSZ0xr57Y00dvjpP
-      config.consumer_secret     = nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK
-      config.access_token        = 804940698684256256-xccKdwwhoErpxCZ0vqOgelB5x8CSQzd
-      config.access_token_secret = aN7S2ysSNE9l0QbzPZKecgmtJt6VftpysanPg1QcKGzQ4
+      config.consumer_key        = "q6YVpFPdsdSZ0xr57Y00dvjpP"
+      config.consumer_secret     = "nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK"
+      config.access_token        = "804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL"
+      config.access_token_secret = "0FY9KuD8l3CJD1crw2wiDJK3vGn1obFMomzITosM5Z8PM"
  end
 
- client.update(testtest)
+# client.update("testtest")
+
+puts "#{client.user_search("高知高専")}"
