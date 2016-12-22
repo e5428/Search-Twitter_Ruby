8c11a030ece0760f969609f2e994ee43870b6e21
Tokaji_Nanami_e5428 <e5428@gm.kochi-ct.jp>
2016-12-22 21:24:09 +0900

    2016_12_22
    each_lineが使えない?

--- TSearch.rb
+++ TSearch.rb
@@ -1,45 +1,42 @@
-# 2016_12_19
+# 2016_12_22
 
-require 'rubygems'
+require 'pp'
 require 'twitter'
+require 'net/http'
+require 'uri'
 require 'oauth'
-require 'json'
 
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
- #client = Twitter::REST::Client.new do |config|
- #     config.consumer_key        = "q6YVpFPdsdSZ0xr57Y00dvjpP"
- #     config.consumer_secret     = "nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK"
- #     config.access_token        = "804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL"
- #     config.access_token_secret = "0FY9KuD8l3CJD1crw2wiDJK3vGn1obFMomzITosM5Z8PM"
- #end
+consumer_key = 'q6YVpFPdsdSZ0xr57Y00dvjpP'
+consumer_secret = 'nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK'
+access_token = '804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL'
+access_token_secret = '0FY9KuD8l3CJD1crw2wiDJK3vGn1obFMomzITosM5Z8PM'
 
-# puts "#{client.user_search("高知高専")}"
-
-# Net::HTTP.get_print(URI.parse('https://api.twitter.com/1.1/users/lookup.json?description=twitter'))
-
-consumer_key   =  'q6YVpFPdsdSZ0xr57Y00dvjpP'
-consumer_secret  =  'nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK'
-access_token   =  '804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL'
-access_token_secret   =  '0FY9KuD8l3CJD1crw2wiDJK3vGn1obFMomzITosM5Z8PM'
-
-
-consumer = OAuth::Consumer.new(consumer_key,consumer_secret,
-                               {
-                                   :site          =>  'http://api.twitter.com' ,
-                                   :scheme    =>  :header
-                               }
+consumer = OAuth::Consumer.new(
+    consumer_key,
+    consumer_secret,
+    site:'https://api.twitter.com/'
 )
 
-token_hash = {
-    :access_token               =>  access_token,
-    :access_token_secret    =>  :header
-}
+endpoint = OAuth::AccessToken.new(consumer, access_token, access_token_secret)
 
-request_token = OAuth::AccessToken.from_hash(consumer,token_hash)
+response = endpoint.get('https://api.twitter.com/1.1/users/search.json?q=twitter')
+result = JSON.parse(response.body)
 
- response = request_token.request(:get, 'https://api.twitter.com/1.1/users/lookup.json?q=twitter')
+# pp result
 
-tweethash = JSON.parse(response.body)
+# result.each_line do |line|
+#   if /id/ =~ line
+#     puts line
+#   end
+# end
 
-puts tweethash
+# TSearch.rb:29:in `<main>': undefined method `each_line' for #<Array:0x000000038e5af0> (NoMethodError)
+#Did you mean?  each_index
+#               each_slice
+#               each
+#               each_with_index
+#               each_entry
+#               each_cons
+
