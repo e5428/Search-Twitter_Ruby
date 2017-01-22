995e06a26cc3d68325282f3fb5bb91fde66ec452
Tokaji_Nanami_e5428 <e5428@gm.kochi-ct.jp>
2017-01-22 12:40:02 +0900

    2017_01_22
    idが何行も表示される => 文字数をカウントして~文字以下のみ出力できないか?

--- TSearch.rb
+++ TSearch.rb
@@ -1,4 +1,4 @@
-# 2017_01_18
+# 2017_01_22
 
 require 'twitter'
 require 'net/http'
@@ -6,14 +6,13 @@ require 'uri'
 require 'oauth'
 
 print"検索ワードを入力 => "
+ search = gets.chomp
 
-search = gets.chomp
+OpenSSL::SSL::VERIFY_PEER
 
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-
-consumer_key = 'q6YVpFPdsdSZ0xr57Y00dvjpP'
-consumer_secret = 'nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK'
-access_token = '804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL'
+consumer_key        = 'q6YVpFPdsdSZ0xr57Y00dvjpP'
+consumer_secret     = 'nce4C4FL3QreA00chktYInrXNVd3gM4LX1fk9Zy9PpW7H3bOrK'
+access_token        = '804940698684256256-ASzNNL5KJvWzVhXIMZep9W9uuCpJ8UL'
 access_token_secret = '0FY9KuD8l3CJD1crw2wiDJK3vGn1obFMomzITosM5Z8PM'
 
 consumer = OAuth::Consumer.new(
@@ -26,15 +25,16 @@ endpoint = OAuth::AccessToken.new(consumer, access_token, access_token_secret)
 
 str = URI.encode_www_form_component("#{search}")
 
-uri = ("https://api.twitter.com/1.1/users/search.json?q=" + str + "&count=1")
+uri = ("https://api.twitter.com/1.1/users/search.json?q=" + str + "&count=3")
 
 response = endpoint.get(uri)
  result = JSON.parse(response.body)
 
-ary = result.to_s.split(",") # 配列 -> 文字列 -> 配列
+ary = result.to_s.split(",")
 
- ary.each do |line|
-   if /"id"=>/ =~ line
-     puts line
-   end
- end
+ary.each do |line|
+  if /{"id"=>/ =~ line
+    puts line
+  end
+end
+
